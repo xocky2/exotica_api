ede2acf42b476124a33a3e40ac0907e363b4b01b
Giovanni Barros <giovanniphilotech@gmail.com>
2022-05-19 19:23:12 -0300

    add img array to the product

--- routes/products.js
+++ routes/products.js
@@ -18,35 +18,39 @@ const upload = multer({storage: storage});
 
 router.get('/', async(req,res,next)=>{
 
-    const result = await mysql.execute("select * from products;");
-    let string =JSON.stringify(result);
-    let json =JSON.parse(string);
-    let produtos = result;
+    const resultProduct = await mysql.execute("select * from product;");
+    let string =JSON.stringify(resultProduct);
+    let jsonProduct =JSON.parse(string);
+    let produtos = resultProduct;
 
-    const result2 = await mysql.execute(`select url from images where products_idproducts = ?;`,[result[0].idproducts]);
+    let tamanho = jsonProduct.length-1;
+    console.log(tamanho)
     
-    let imgs = [];
-    result2.forEach(images2 => {
-        imgs.push(images2.url)
-    });
+    for (let index = 0; index <= tamanho; index++) {
+        const result2 = await mysql.execute(`select url from image where product_idproduct = ?;`,[jsonProduct[index]['idproduct']]);
+        let string2 =JSON.stringify(result2);
+        let json2 =JSON.parse(string2);
+
+        let imgs = [];
+        json2.forEach(json2 => imgs.push(json2['url']) ); //cria array com as urls das imagens
+        jsonProduct[index].images = imgs;
+
+    }
     
-
-
-    let string2 =JSON.stringify(result2);
-    let json2 =JSON.parse(string2);
-
-    const response = {
+   let  response = {
         status: 200,
-        products: json.map(product =>{
+        products: jsonProduct.map(product =>{
             return {
                 id: product.idproducts,
                 name: product.name,
-                categoria: product.categoria,
+                category: product.category,
                 price: product.price,  
-                images: imgs          
+                images: product.images         
             }
         })
     }
+
+    // cria array com url das imgs 
 
     return res.status(200).send({response});
 });
