2de027b6dc045320c4393d16b9e83cb88c5a20da
Giovanni Barros <giovanniphilotech@gmail.com>
2022-05-25 19:57:33 -0300

    config heroku

--- app.js
+++ app.js
@@ -1,13 +1,14 @@
 const express = require('express');
 const app = express();
-
 const routeProducts = require('./routes/products');
+const cors = require('cors');
 
 app.use('/uploads', express.static('uploads'));
 app.use(express.urlencoded({ extended: false })) //apenas dados simples
 app.use(express.json())//apenas json de entrada no body
 
 app.use('/products', routeProducts);
+app.use(cors());
 
 app.use((req,res,next)=>{
     const erro = new Error('Ops, rota não encontrada');
--- routes/products.js
+++ routes/products.js
@@ -40,7 +40,6 @@ router.get('/', async(req,res,next)=>{
 
         //adiciona estoque
         for (let index = 0; index <= tamanho; index++) {
-            console.log(jsonProduct[index]['idproduct'])
             const resultStock = await mysql.execute(`select size,quantity from stock where product_idproduct=?;`,[jsonProduct[index]['idproduct']]);
             let stringStock =JSON.stringify(resultStock);
             let jsonStock =JSON.parse(stringStock);
@@ -58,6 +57,7 @@ router.get('/', async(req,res,next)=>{
                 return {
                     id: product.idproducts,
                     name: product.name,
+                    description: product.description,
                     category: product.category,
                     price: product.price,  
                     main_image: product.main_image,
@@ -78,11 +78,54 @@ router.get('/', async(req,res,next)=>{
 
 
 router.post('/', async(req,res,next)=>{
-    const result = await mysql.execute("select * from products;");
-    console.log(typeof result);
-    let string =JSON.stringify(result);
-    let json =JSON.parse(string);
-    return res.status(200).send(json);
+    try {
+        if (req.body.name == null){
+            return res.status(201).send({response: "Please enter a name for the product !! "});
+        }else{
+            const resultInsertProduct = await mysql.execute(`insert into product (name,category,price,main_image,description,visible) values (?,?,?,?,?,?)`,
+                        [req.body.name,req.body.category,req.body.price,req.body.main_image,req.body.description,req.body.visible]);
+            if(resultInsertProduct){
+                let imgs = req.body.images;
+                imgs.forEach(async imgs  => {
+                    console.log(imgs);
+                    const resultInsertedImage = await mysql.execute(`insert into image (url,product_idproduct) values (?,?);`,
+                        [imgs,resultInsertProduct.insertId]);
+
+                    })     
+                
+                
+                
+                const resultInsertedProduct = await mysql.execute(`select name,category,price,main_image,description,visible from product where idproduct = ?;`,
+             [resultInsertProduct.insertId]);
+                if(resultInsertedProduct){
+                    const responseInsertedProduct = {
+                        status: 200,
+                        product: resultInsertedProduct.map(product =>{
+                            return {
+                                id: product.idproducts,
+                                name: product.name,
+                                description: product.description,
+                                category: product.category,
+                                price: product.price,  
+                                main_image: product.main_image,
+                                //images: product.images
+                              //  stock: product.stock       
+                            }
+                        })
+
+                    }
+                    return res.status(201).send({responseInsertedProduct});
+                }else{
+                    return res.status(401).send({response: {status: 401,
+                    msg: "Product not inserted"}});
+                }
+            } // end if se cadastrou
+
+        }
+    } catch (error) {
+        return res.status(500).send({error: error});
+    }
 });
+
 
 module.exports = router;
