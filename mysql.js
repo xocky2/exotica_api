90de377b40cb88e3287ef09066e99df9f7812884
Giovanni Barros <giovanniphilotech@gmail.com>
2022-05-25 19:59:57 -0300

    config heroku

--- mysql.js
+++ mysql.js
@@ -12,7 +12,7 @@ const mysql = require('mysql');
 
 var pool = mysql.createPool({
     "user": process.env.MYSQL_USER,
-    //"password": process.env.MYSQL_PASSWORD,
+    "password": process.env.MYSQL_PASSWORD,
     "database": process.env.MYSQL_DATABASE,
     "host": process.env.MYSQL_HOST,
     "port" : process.env.MYSQL_PORT,
