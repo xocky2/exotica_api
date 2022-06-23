62425aaca94a8620c462fc262805b988b8cecfa7
Giovanni Barros <giovanniphilotech@gmail.com>
2022-06-22 21:37:06 -0300

    Add cors method

--- app.js
+++ app.js
@@ -22,7 +22,7 @@ app.use((req, res, next) => {
 
     if (req.method === 'OPTIONS') {
         res.header('Access-Control-Allow-Methods', 'PUT, POST, PATCH, DELETE, GET');
-        return res.status(200).send({});
+        return res.status(200).send("Ta enviando o OPTIONS PORRA");
     }
     next();
 });
