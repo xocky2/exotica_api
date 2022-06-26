6a6e387514fca022c6dd17fd7380f55f64d24938
Giovanni Barros <giovanniphilotech@gmail.com>
2022-06-26 16:32:22 -0300

    Remove name validation

--- app.js
+++ app.js
@@ -15,10 +15,6 @@ app.use(express.json())//apenas json de entrada no body
 
 app.use((req, res, next) => {
     res.header('Access-Control-Allow-Origin', '*');
-    res.header(
-        'Access-Control-Allow-Header',
-        'Content-Type, Authorization'
-    );
 
     if (req.method === 'OPTIONS') {
         res.header('Access-Control-Allow-Methods', 'PUT, POST, PATCH, DELETE, GET');
